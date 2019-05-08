ae3ec8ab28d0883df901b7f08ca72aea0b48de3f
adarpis <aarpi@procelec.ec>
2019-05-08 16:37:56 -0500

    first demo versión v0.01

--- src/lidar2osc.py
+++ src/lidar2osc.py
@@ -7,16 +7,27 @@ __author__ = "Adrian Arpi <aarpi@imaytec.com>"
 __date__ = "20 February 2019"
 
 __credits__ = """Guido van Rossum, for an excellent programming language.
-Edy Ayala and Universidad Politecnica Salesiana, for all his support on this.
+Edy Ayala and Universidad Politecnica Salesiana, for all theirs support on this.
 """
 
 # Known bugs that can't be fixed here:
 #   - 
 
+import sys
+from time import sleep
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
 
 if __name__ == '__main__': 
-    sender.send_message('/trigger/prophet', [70, 100, 8])
+    while True:
+        try:
+            for note in range(40, 120, 10):
+                sender.send_message('/trigger/prophet', [note, 100, 0.5])
+                sleep(0.5)
+            for note in range(120, 40, -10):
+                sender.send_message('/trigger/prophet', [note, 100, 0.5])
+                sleep(0.5)
+        except KeyboardInterrupt:
+            sys.exit(0)
